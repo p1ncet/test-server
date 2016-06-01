b41b3e52560ecd9cd7cef1f66cadc6137f69c583
Valeriy Pudul <valery.pudul@gmail.com>
2016-06-01 19:14:02 +0300

    Fix: autoload

--- src/response.php
+++ src/response.php
@@ -2,7 +2,15 @@
 
 namespace TServer;
 
-require_once __DIR__ . "/../vendor/autoload.php";
+$path = "/..";
+while (is_dir(__DIR__ . $path) && !is_dir(__DIR__ . "$path/vendor") && strlen($path) < 20) {
+	$path .= "/..";
+}
+if (file_exists(__DIR__ . "$path/vendor/autoload.php")) {
+	require_once __DIR__ . "$path/vendor/autoload.php";
+} else {
+	throw new \LogicException("Vendor dir not found");
+}
 
 if (isset($_ENV['handler']) && class_implements($_ENV['handler'], Responsible::class)) {
 	/** @var Responsible $handler */
