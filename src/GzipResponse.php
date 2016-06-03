7e6f9b3ca685e549579024a63caf60d9ca24ae89
Valeriy Pudul <valery.pudul@gmail.com>
2016-06-03 21:04:04 +0300

    Fix: GzipResponse

--- src/GzipResponse.php
+++ src/GzipResponse.php
@@ -5,7 +5,9 @@ namespace Testo;
 class GzipResponse extends EchoResponse {
 	public function response() {
 		header("Content-Encoding: gzip");
-		$content = gzcompress(parent::response());
+//		$content = gzcompress(parent::response());
+//		$content = gzinflate(parent::response());
+		$content = gzencode(parent::response());
 //		header("Content-Length: " . mb_strlen($content));
 		return $content;
 	}
